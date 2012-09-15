3cccd04b79c510923518930d64cedd6d7ade6b41
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2012-09-15 13:05:15 -0400

    Adding a StopMotion Maker class

--- week1-movingRec/src/StopMotion.cpp
+++ week1-movingRec/src/StopMotion.cpp
@@ -7,3 +7,51 @@
 //
 
 #include "StopMotion.h"
+
+StopMotion::StopMotion(){
+    
+}
+
+void StopMotion::allocate( int _width, int _height ){
+    width = _width;
+    height = _height;
+}
+
+void StopMotion::load(string _path){
+    
+}
+
+void StopMotion::save(string _path){
+    for(int i = 0; )
+}
+
+void StopMotion::addFrame( unsigned char * _pixels ){
+    
+    //  If is not frames means it's the first one
+    //
+    if (buffer.size() == 0){
+        startTime = ofGetElapsedTimeMillis();
+    }
+    
+    Frame newFrame;
+    newFrame.pixels = _pixels;
+    newFrame.timeStamp = ofGetElapsedTimeMillis() - startTime;
+    
+    buffer.push_back( newFrame );
+}
+
+void StopMotion::nextFrame(){
+    
+}
+
+void StopMotion::goFrame(){
+    
+}
+
+void StopMotion::goPct(float _pct){
+    
+}
+
+void StopMotion::draw(int _x, int _y, int _width, int _height){
+    
+}
--- week1-movingRec/src/StopMotion.h
+++ week1-movingRec/src/StopMotion.h
@@ -6,9 +6,40 @@
 //
 //
 
-#ifndef week1_movingRec_StopMotion_h
-#define week1_movingRec_StopMotion_h
+#ifndef STOPMOTION
+#define STOPMOTION
 
+#include "ofMain.h"
 
+typedef struct{
+    unsigned char * pixels;
+    unsigned long timeStamp;
+} Frame;
+
+class StopMotion {
+public:
+
+    StopMotion();
+    
+    void    allocate( int _width, int _height );
+    void    load(string _path);
+    void    save(string _path);
+    
+    void    addFrame( unsigned char * _pixels );
+    
+    void    nextFrame();
+    void    goFrame();
+    
+    void    goPct(float _pct);
+    
+    void    draw(int _x, int _y, int _width = -1, int _height = -1);
+    
+private:
+    vector< Frame > buffer;
+    
+    unsigned long startTime;
+    int     width, height;
+    
+};
 
 #endif
