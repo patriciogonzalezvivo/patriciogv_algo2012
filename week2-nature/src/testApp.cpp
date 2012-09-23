8d92cf1c69c8508bc24695817e9b62fccfb7cc5f
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2012-09-23 19:36:27 -0400

    trying to make simple waves on 2D

---
+++ week2-nature/src/testApp.cpp
@@ -0,0 +1,99 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+    ofEnableAlphaBlending();
+    ofSetVerticalSync(true);
+    ofEnableSmoothing();
+    
+    for(int i = 0; i < BUFFER_SIZE ; i++  ){
+        bufferA[i] = 0.0;
+        bufferB[i] = 0.0;
+    }
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+    
+    //  Trying to apply this ( http://www.patriciogonzalezvivo.com/blog/?p=657 )
+    //  y use on a shader but in one dimension
+    //
+    for(int i = 1; i < BUFFER_SIZE-1 ; i++  ){
+        bufferB[i] = (bufferA[i+1]+bufferA[i-1])*0.5;
+        bufferB[i] = bufferB[i]*0.5 - bufferA[i];
+        bufferB[i] *= 0.68;
+    }
+    
+    for(int i = 0; i < BUFFER_SIZE ; i++  ){
+        bufferA[i] = bufferB[i];
+        
+        if (bufferA[i] > PI*2){
+            bufferA[i] -= PI*2;
+        }
+    }
+    
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+    ofBackground(0);
+    
+    ofNoFill();
+    ofSetColor(255);
+    
+    ofBeginShape();
+    ofVertex(0,ofGetHeight()*0.5);
+    float dist = ofGetWidth()/BUFFER_SIZE;
+    for (int i = 0; i < BUFFER_SIZE; i++) {
+        ofCurveVertex(i*dist, ofGetHeight()*0.5 + sin( bufferA[i] )*50 );
+    }
+    ofVertex(ofGetWidth(),ofGetHeight()*0.5);
+    ofEndShape();
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+    int index = ofMap(x, 0, ofGetWidth(), 0,BUFFER_SIZE, true);
+    bufferA[ index ] = PI*1.5;
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ week2-nature/src/testApp.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "ofMain.h"
+#define BUFFER_SIZE 100
+
+class testApp : public ofBaseApp{
+public:
+    void setup();
+    void update();
+    void draw();
+
+    void keyPressed  (int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+		
+    float bufferA[BUFFER_SIZE];
+    float bufferB[BUFFER_SIZE];
+};
