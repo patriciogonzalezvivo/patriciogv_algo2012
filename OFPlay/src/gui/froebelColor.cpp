01f20c067546980aaeb6d40498beef26ffe90727
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2012-12-03 21:37:43 -0500

    previus

---
+++ OFPlay/src/gui/froebelColor.cpp
@@ -0,0 +1,9 @@
+//
+//  froebelColor.cpp
+//  OFPlay
+//
+//  Created by Patricio Gonzalez Vivo on 12/3/12.
+//
+//
+
+#include "froebelColor.h"
---
+++ OFPlay/src/gui/froebelColor.h
@@ -0,0 +1,14 @@
+//
+//  froebelColor.h
+//  OFPlay
+//
+//  Created by Patricio Gonzalez Vivo on 12/3/12.
+//
+//
+
+#ifndef __OFPlay__froebelColor__
+#define __OFPlay__froebelColor__
+
+#include <iostream>
+
+#endif /* defined(__OFPlay__froebelColor__) */
